564c53986388d444c1258bdd1f00e9d79ceea00d
Yurii Miroshnyk <lemadioro@gmail.com>
2024-09-18 14:58:09 +0300

    Add redirect to the login page if user not logined

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controllers;
 
+use Core\Auth;
 use Core\Controller;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        if (!Auth::check()) {
+            header('Location: /login');
+
+            return false;
+        }
+
         return $this->view('home');
     }
 }
